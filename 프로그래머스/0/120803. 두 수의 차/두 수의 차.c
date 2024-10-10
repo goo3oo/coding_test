957ec7b04c403274cf23abccff9722e0bbc0f890
goo3oo <goo3oo0918@gmail.com>
2024-10-10 19:26:32 +0900

    [level 0] Title: 두 수의 차, Time: 0.02 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+    int answer = num1-num2;
+    scanf("%d %d", &num1, &num2);
+    if(-50000<=num1 && num1<=50000 && -50000<=num2 && num2<=50000)
+    return answer;
+ }
+
+
+
