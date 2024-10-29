6d452efd34ab81dce2ee34c87cdcc0cb24638982
goo3oo <goo3oo0918@gmail.com>
2024-10-29 23:35:19 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.07 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,16 +1,16 @@
 class Solution {
-    public int solution(int num) {
-           int answer = 0;
-           long longNum = (long)num;
+    public long solution(long num) {
+           long answer = 0;
 
-        while (longNum != 1){
-            if(answer >=500){
-                return -1;
+        while (num != 1) {
+            if (answer >= 500) { // 500번 이상 반복되면 -1을 반환
+                answer =  -1;
             }
-            if(longNum%2==0){
-                longNum/=2;
-            }else{
-                longNum=(longNum*3)+1;
+
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num = num * 3 + 1;
             }
             answer++;
         }
