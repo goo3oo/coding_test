3e091bc3446911926fa703a1b9e96e18ed81f1d6
goo3oo <goo3oo0918@gmail.com>
2025-01-15 11:13:45 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,11 +1,37 @@
 import java.util.Calendar;
 
 class Solution {
+    
+    private final int[] DAYS_IN_MONTH_NORMAL = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private final int[] DAYS_IN_MONTH_LEAP = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private final String[] WEEKDAYS = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+    
+    private boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+
     public String solution(int a, int b) {
-      String[] weekdays = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(2016, a-1, b);
-        int datOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
-        return weekdays[datOfWeek - 1];
+        int year = 2016;
+        int[] daysInMonth = isLeapYear(year) ? DAYS_IN_MONTH_LEAP : DAYS_IN_MONTH_NORMAL;
+        String yearStartDay = "FRI";
+
+        int addDay = 0;
+        for (int i = 0; i < WEEKDAYS.length; i++) {
+            if(WEEKDAYS[i].equals(yearStartDay)) {
+                addDay = i;
+                break;
+            }
+        }
+
+        int totalDays = 0;
+        for (int i = 0; i < a-1 ; i++) {
+            totalDays += daysInMonth[i];
+        }
+
+        totalDays += b;
+
+       int dayIndex = (addDay + totalDays-1) % 7;
+
+       return WEEKDAYS[dayIndex];
     }
 }
